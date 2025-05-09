ca15c8d5dc3319104e5c71c09329c1db407c4038
anthony.mcginn <anthony.mcginn@unclass.factory.subs>
2025-05-09 12:55:12 +0100

    Update

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,6 +34,33 @@ resource "kind_cluster" "default" {
     }
 }
 
+resource "docker_image" "dnsmasq" {
+    name    = "jpillora/dnsmasq:latest"
+}
+
+resource "docker_container" "dnsmasq" {
+  name      = "dnsmasq"
+  image     = docker_image.dnsmasq.name
+  restart   = "unless-stopped"
+
+  ports {
+    internal = 53
+    external = 53
+    protocol = "tcp"
+  }
+
+  ports {
+    internal = 53
+    external = 53
+    protocol = "udp"
+  }
+
+  volumes {
+    host_path      = abspath("${path.module}/files/dnsmasq/dnsmasq.conf")
+    container_path = "/etc/dnsmasq.conf"
+  }
+}
+
 # resource "docker_network" "private_network" {
 #     name    =   var.vlan_name
 #     driver  =   var.vlan_driver
